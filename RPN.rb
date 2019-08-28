76be07c1a2a8d360c4c59c8391f50af9c99d50e8
github username <jpsypek@gmail.com>
2019-08-27 21:03:31 -0600

    build initial functionality without edge case prevention

--- RPN.rb
+++ RPN.rb
@@ -1,3 +1,27 @@
 class Calculator
 
+  attr_reader :values
+
+  def initialize(value)
+    @values = [value]
+  end
+
+  def add_value(value)
+    @values << value
+  end
+
+  def perform_calculation(operator)
+    case operator
+    when "+"
+      calculated_value = @values.reduce(&:+)
+    when "-"
+      calculated_value = @values.reduce(&:-)
+    when "*"
+      calculated_value = @values.reduce(&:*)
+    when "/"
+      calculated_value = @values.reduce(&:/)
+    end
+    @values = [calculated_value]
+    STDOUT.puts "= #{calculated_value}"
+  end
 end
--- app.rb
+++ app.rb
@@ -1,15 +1,38 @@
 require_relative './RPN.rb'
 
 def welcome
-  STDOUT.puts "Welcome to the RPN calculator. To get started, please enter a number"
-  number = STDIN.gets.chomp()
-  case number
-  when "1"
-    STDOUT.puts "You entered 1"
-    welcome
-  when "exit"
+  STDOUT.puts "Welcome to the RPN calculator. To get started, please enter a number:"
+  first_value
+end
+
+def first_value
+  input = STDIN.gets.chomp()
+  if input.to_i.to_s == input
+    $calc = Calculator.new(input.to_i)
+    values_after_initialized(input.to_i)
+  elsif input == "q"
     STDOUT.puts "goodbye"
     exit
+  else
+    STDOUT.puts "Please enter a number"
+    first_value
+  end
+end
+
+def values_after_initialized(previous_value)
+  input = STDIN.gets.chomp()
+  if input.to_i.to_s == input
+    $calc.add_value(input.to_i)
+    values_after_initialized
+  elsif input == "q"
+    STDOUT.puts "goodbye"
+    exit
+  elsif input == "+" || input == "-" || input == "*" || input == "/"
+    $calc.perform_calculation(input)
+    values_after_initialized
+  else
+    STDOUT.puts "Please enter a number or an operator:"
+    values_after_initialized
   end
 end
 
